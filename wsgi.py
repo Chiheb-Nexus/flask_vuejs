d11bdc85423f9804e613e1086284ddd35686a055
chiheb-nexus <chihebnexus@gmail.com>
2020-06-07 04:37:34 +0200

    Refactoting

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,12 @@
 # WSGI entry point
 #
 
-from models import application
+from backend.models import application
 
 PORT = 8080
 HOST = '127.0.0.1'
 
 
 if __name__ == '__main__':
-    from urls import *  # noqa: F401,F403
+    from backend.urls import *  # noqa: F401,F403
     application.run(port=PORT, host=HOST)
